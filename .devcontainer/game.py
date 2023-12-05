b2086ddc800906e1288680d551e5c66de31582b9
sugary2023 <150818257+sugary2023@users.noreply.github.com>
2023-12-05 13:24:34 +0000

    bruh

---
+++ .devcontainer/game.py
@@ -0,0 +1,22 @@
+import pyinputplus as pyip
+import random
+min = 0
+max = 100
+count = 0
+random_number=random.randint(min,max)
+print("=========猜數字遊戲=========")
+while True:
+    keyin = pyip.inputInt(f"猜數字範圍{min}~{max}:")
+    count += 1
+    print(keyin)
+    if keyin == random_number:
+        print(f"Bingo! Answer is:{random_number}")
+        print(f"您猜了{count}次")
+        break
+    elif keyin > random_number:
+        print("再小一點")
+    elif keyin < random_number:
+        print("再大一點")
+        min = keyin + 1
+    print(f"您已經猜了{count}次")        
+print("遊戲結束")    
