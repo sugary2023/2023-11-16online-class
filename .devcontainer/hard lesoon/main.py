6d31e20b406e549f4f0c3150ce366a8a02a1138b
sugary2023 <150818257+sugary2023@users.noreply.github.com>
2023-12-21 13:07:23 +0000

    yt

--- .devcontainer/hard lesoon/main.py
+++ .devcontainer/hard lesoon/main.py
@@ -1,12 +1,39 @@
+from random import randint,choices
+import csv
+from csv import DictWriter
 import pyinputplus as pyip
-from tool import getStudents,saveToCSV
 
-if __name__ == '__main__':
+def getStudents(nums:int) -> list[dict]:  
+    students:list[dict] = []
+    with open('names.txt',mode='r',encoding='utf-8') as file:
+        names:str = file.read()
+    nameList:list[str] = names.split('\n')
+    names:list[str] = choices(nameList,k=nums)
+
+    for i in range(nums):
+        stu = {
+        '姓名':names[i],
+        '國文':randint(45,100),
+        '英文':randint(45,100),
+        '數學':randint(45,100),
+        '地理':randint(45,100),
+        '歷史':randint(45,100),
+        }
+        students.append(stu)
+        
+    return students
+
+def save_to_csv(students:list[dict],fileName:str)->None:    
+    fileNameWithExtension:str = fileName + '.csv'
+    with open(fileNameWithExtension,mode='w',encoding='utf-8',newline='') as file:
+        fieldnames:list[str] = ['姓名', '國文', '英文', '數學', '地理', '歷史']
+        writer:DictWriter = csv.DictWriter(file,fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(students)
+    print("寫入成功")
+
+if __name__ == "__main__":
     s_nums:int = pyip.inputInt("請輸入學生的人數(1~50):",min=1,max=50)
-    o_nums:int = pyip.inputInt("請輸入科目數(1~7):",min=1,max=7)
-    students:list[list] = getStudents(student_nums=s_nums,scores_nums=o_nums)
+    students:list[dict] = getStudents(s_nums)
     fileName = pyip.inputFilename("請輸入檔案名稱(不用輸入副檔名稱):")
-    if saveToCSV(fileName=fileName,data=students,subject_nums=o_nums):
-        print("存檔成功")
-    else:
-        print("存檔失敗")
+    save_to_csv(students,fileName)
